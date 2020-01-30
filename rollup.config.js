54c90a77da4b1590f01dff62525e6f981b4d338a
Justin Foley <justin@modweb.io>
2020-01-30 11:51:29 -0500

    fix rollup for deploys

--- rollup.config.js
+++ rollup.config.js
@@ -74,8 +74,8 @@ export default {
     // If we're building for production (npm run build
     // instead of npm run dev), minify
     production && terser(),
-    serve("public"),
-    livereload()
+    !production && serve("public"),
+    !production && livereload()
   ],
   watch: {
     clearScreen: true
